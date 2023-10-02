62502b28c50b5035a748db3dc2aada254e881f00
Andres Hiis <andres.hiis@tktk.ee>
2023-10-02 14:44:54 +0300

    add week4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,8 @@
+﻿int sum = 0;
+for(int i = 1; i < 4; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;
+}
+//Console.WriteLine($"Final total {sum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,5 @@
+﻿for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+Console.WriteLine("Have a nice day!");
